05203fb2192926157da4b490da745d754643117e
leeyunjai <yunjai.lee.82@gmail.com>
2020-01-08 08:13:49 +0900

    update

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 const readline = require('readline');
-const Oled = require('./spi-oled');
+const Oled = require('spi-oled');
 const oled = new Oled({ width: 128, height: 64, dcPin: 23, rstPin : 24});
 
 oled.begin(function(){
