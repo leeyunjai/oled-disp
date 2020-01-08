c7363f24ed2329a2a72976c455b2d32ebd704be4
leeyunjai <yunjai.lee.82@gmail.com>
2020-01-08 09:43:41 +0900

    update

--- test.js
+++ test.js
@@ -3,6 +3,17 @@ const oled = new Oled({ width: 128, height: 64, dcPin: 23, rstPin : 24});
 
 oled.begin(function(){
   oled.clearDisplay();
+  
+  // hangul(kor) and eng
   oled.setCursor(1, 1);
   oled.writeString(2, "안녕,Hi", 2, true);
+ 
+  // install png-to-lcd and only 128x64 png file
+  /*
+  const png = require('png-to-lcd');
+  png("test.png", false, function(err, data){
+    oled.buffer = data
+    oled.update()
+  });
+  */
 });
